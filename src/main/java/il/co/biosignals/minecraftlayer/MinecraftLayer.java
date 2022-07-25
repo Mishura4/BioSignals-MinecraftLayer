e5950f897869455c4c9f7b747ceaf6c4e423c99b
Amber Ehrlich <miuna.oshino@gmail.com>
2022-07-25 09:31:27 -0400

    Added project structure.

---
+++ src/main/java/il/co/biosignals/minecraftlayer/MinecraftLayer.java
@@ -0,0 +1,13 @@
+package il.co.biosignals.minecraftlayer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftLayer extends JavaPlugin
+{
+  @Override
+  public void onEnable() {
+  }
+  @Override
+  public void onDisable() {
+  }
+}
